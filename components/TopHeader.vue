f131522d6daa2eadaaa3f55595b222b7da856958
teambit710 <justtrustbit710@gmail.com>
2020-06-15 23:50:28 -0700

    initial commit

---
+++ components/TopHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="top-header">
+    <div class="bg-gray-700 flex flex-wrap">
+      <div class="">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
